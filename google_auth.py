41eabaa2c78abce834a3afc4cb00011a97dfe12c
Patrick Zhou <pzhou07920@gmail.com>
2025-09-10 22:43:27 -0400

    read secrets from local file

--- google_auth.py
+++ google_auth.py
@@ -26,8 +26,7 @@ def google_auth():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file('client_secret.json', scopes=SCOPES)
-            # flow.redirect_uri = 'http://localhost/'
-            creds = flow.run_local_server(port=8000, prompt='consent')
+            creds = flow.run_local_server(port=8001, prompt='consent')
         # Save the credentials for the next run
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
--- streaming-api.py
+++ streaming-api.py
@@ -5,17 +5,26 @@ import subprocess
 import requests
 import google_auth as ga
 from time import sleep
+import yaml
 
 app = FastAPI()
 
 @app.get("/stream")
 async def stream():
+    # read in secrets.yml
+    with open("secrets.yml", "r") as f:
+        secrets = yaml.safe_load(f)
+    stream_username = secrets['stream_username']
+    stream_password = secrets['stream_password']
+    stream_name = secrets['stream_names'][0]
+     #print(f"testing {secrets['stream_names'][0]}")
+
     access_code = ac.generate_access_code()
     ga.start_yt_broadcast()
     sleep(5)
     subprocess.run(['ffmpeg',
                     '-i',
-                    'rtsp://admin:spot9666@192.168.50.215/Preview_01_main',
+                    'rtsp://{stream_username}:{stream_password}@192.168.50.215/{stream_name}',
                     '-vcodec',
                     'copy',
                     '-acodec',
@@ -23,8 +32,6 @@ async def stream():
                     '-f',
                     'flv',
                     'rtmp://a.rtmp.youtube.com/live2/4zh4-9gbe-zyg6-qqhx-8vxs'
-                    ])
-
-    response = requests.post("https://www.googleapis.com/youtube/v3/liveBroadcasts")
+                    ], capture_output=False)
 
     return f"Today's access_code = {access_code}"
