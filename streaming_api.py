844f09ac18c87ad26c7aea09c230a4b172abecca
Patrick Zhou <pzhou07920@gmail.com>
2025-09-12 02:07:33 -0400

    multistream endpoint for testing

--- streaming_api.py
+++ streaming_api.py
@@ -16,15 +16,19 @@ async def stream(stream_name: str = Query(None)):
     broadcast_id = sf.create_stream(stream_name, secrets)
     return f"Stream has been started! Watch the stream here: https://www.youtube.com/live/{broadcast_id}"
 
-@app.get("/test_stream")
-async def test_stream():
+@app.get("/test_multi_streams")
+async def test_multi_streams(stream_count: int = Query(None)):
     # read in secrets.yml
     with open("secrets.yml", "r") as f:
         secrets = yaml.safe_load(f)
 
+    ga.google_auth()
+    print(f"Testing {stream_count} streams")
+    count = 0
     for stream_name in secrets['stream_names']:
-        print(f"Starting stream: {stream_name}")
-        stream_id = sf.create_stream(stream_name, secrets)
-        sleep(20)
-    print(f"Stream has been started! Watch the stream here: https://www.youtube.com/live/{stream_id}")
-    return "Muilti-stream test completed."
+        if count < stream_count:
+            print(f"Starting stream: {stream_name}")
+            broadcast_id = sf.create_stream(stream_name, secrets)
+            sleep(20)
+    print(f"Stream has been started! Watch the stream here: https://www.youtube.com/live/{broadcast_id}")
+    return "Multi-stream test completed."
